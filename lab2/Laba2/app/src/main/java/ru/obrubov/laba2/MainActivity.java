4afce13740c357ef0bc2badb86c01c40b94c9810
Maxim Obrubov` <obrubov.max@mail.ru>
2018-09-17 22:42:07 +0300

    Добавлены обработчики кнопок

--- lab2/Laba2/app/src/main/java/ru/obrubov/laba2/MainActivity.java
+++ lab2/Laba2/app/src/main/java/ru/obrubov/laba2/MainActivity.java
@@ -2,6 +2,7 @@ package ru.obrubov.laba2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +13,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void on1(View view) {
+    }
+
+    public void on2(View view) {
+    }
+
+    public void on3(View view) {
+    }
+
+    public void on4(View view) {
+    }
+
+    public void on5(View view) {
+    }
+
+    public void on6(View view) {
+    }
+
+    public void on7(View view) {
+    }
+
+    public void on8(View view) {
+    }
+
+    public void on9(View view) {
+    }
+
+    public void on0(View view) {
+    }
+
+    public void onMultiple(View view) {
+    }
+
+    public void onDivide(View view) {
+    }
+
+    public void onMinus(View view) {
+    }
+
+    public void onPlus(View view) {
+    }
+
+    public void onEqual(View view) {
+    }
+
+    public void onDelete(View view) {
+    }
 }
