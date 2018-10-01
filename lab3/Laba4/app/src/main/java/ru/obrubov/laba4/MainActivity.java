61b0484d252e056e2485e60d1483d20e69b32840
Maxim Obrubov` <obrubov.max@mail.ru>
2018-10-01 21:34:49 +0300

    Реализован компас

--- lab3/Laba4/app/src/main/java/ru/obrubov/laba4/MainActivity.java
+++ lab3/Laba4/app/src/main/java/ru/obrubov/laba4/MainActivity.java
@@ -18,12 +18,19 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     TextView magnetZ;
     TextView proximity;
     TextView light;
+    TextView Zrotation;
+    TextView side;
 
     SensorManager sensorManager;
     Sensor accSensor;
     Sensor magnetSensor;
     Sensor proximitySensor;
     Sensor lightSensor;
+
+    float[] accValues;
+    float[] magnetValues;
+    float[] rotationMatrix;
+    float[] orientationData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,8 +42,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         magnetX = findViewById(R.id.magnetXcoord);
         magnetY = findViewById(R.id.magnetYcoord);
         magnetZ = findViewById(R.id.magnetZcoord);
-        proximity = findViewById(R.id.proximity);
-        light = findViewById(R.id.light);
+        proximity = findViewById(R.id.proximitySensorData);
+        light = findViewById(R.id.lightSensorData);
+        Zrotation = findViewById(R.id.ZrotationValue);
+        side = findViewById(R.id.sideText);
 
         sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         if(sensorManager != null) {
@@ -45,6 +54,11 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
             lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         }
+
+        accValues = new float[3];
+        magnetValues = new float[3];
+        rotationMatrix = new float[16];
+        orientationData = new float[3];
     }
 
     @Override
@@ -53,17 +67,38 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             accX.setText(String.valueOf(event.values[0]));
             accY.setText(String.valueOf(event.values[1]));
             accZ.setText(String.valueOf(event.values[2]));
+            accValues = event.values.clone();
         }
         if(event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
             magnetX.setText(String.valueOf(event.values[0]));
             magnetY.setText(String.valueOf(event.values[1]));
             magnetZ.setText(String.valueOf(event.values[2]));
+            magnetValues = event.values.clone();
         }
         if(event.sensor.getType() == Sensor.TYPE_PROXIMITY) {
             proximity.setText(String.valueOf(event.values[0]));
         }
         if(event.sensor.getType() == Sensor.TYPE_LIGHT) {
             light.setText(String.valueOf(event.values[0]));
+        }
+        SensorManager.getRotationMatrix(rotationMatrix, null, accValues, magnetValues);
+        SensorManager.getOrientation(rotationMatrix, orientationData);
+        double zDegrees = Math.toDegrees(orientationData[0]);
+        Zrotation.setText(String.valueOf(zDegrees));
+
+        boolean isNorth = zDegrees > -45 && zDegrees < 45;
+        boolean isEast = zDegrees > 45 && zDegrees < 135;
+        boolean isSouth = (zDegrees >= -180 && zDegrees < -135) || (zDegrees > 135 && zDegrees <= 180);
+        boolean isWest = zDegrees > -135 && zDegrees < -45;
+
+        if(isNorth) {
+            side.setText("Север");
+        } else if (isEast) {
+            side.setText("Восток");
+        } else if (isSouth) {
+            side.setText("Юг");
+        } else if (isWest) {
+            side.setText("Запад");
         }
     }
 
