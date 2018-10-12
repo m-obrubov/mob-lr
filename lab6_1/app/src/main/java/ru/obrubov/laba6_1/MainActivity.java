17a9f7d6b930caefa49a0c7ad780204bb829818f
Maxim Obrubov` <obrubov.max@mail.ru>
2018-10-12 16:31:37 +0300

    Текст и стиои рисования

--- lab6_1/app/src/main/java/ru/obrubov/laba6_1/MainActivity.java
+++ lab6_1/app/src/main/java/ru/obrubov/laba6_1/MainActivity.java
@@ -20,53 +20,38 @@ public class MainActivity extends AppCompatActivity {
 
     class DrawView extends View {
         Paint paint;
-        RectF rectF;
-
-        float[] points;
-        float[] points1;
+        Rect rect;
+        StringBuilder stringBuilder;
 
         public DrawView(Context context) {
             super(context);
             paint = new Paint();
-            rectF = new RectF(700, 100, 800, 150);
-
-            points = new float[] {100, 50, 150, 100, 150, 200, 50, 200, 50, 100};
-            points1 = new float[] {300, 200, 600, 200, 300, 300, 600, 300, 400, 100, 400, 400, 500, 100, 500, 400};
+            rect = new Rect(100, 200, 200, 300);
+            stringBuilder = new StringBuilder();
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
             canvas.drawARGB(80, 102, 204, 255);
-            paint.setColor(Color.RED);
-            paint.setStrokeWidth(10);
-            canvas.drawPoints(points, paint);
-            canvas.drawLines(points1, paint);
-
-            paint.setColor(Color.GREEN);
-            canvas.drawRoundRect(rectF, 20, 20, paint);
-
-            rectF.offset(0, 150);
-            canvas.drawOval(rectF, paint);
-
-            rectF.offsetTo(900, 100);
-            rectF.inset(0, -25);
-            canvas.drawArc(rectF, 90, 270, true, paint);
-
-            rectF.offset(0, 150);
-            canvas.drawArc(rectF, 90, 270, false, paint);
-
-            paint.setStrokeWidth(3);
-            canvas.drawLine(150, 450, 150, 600, paint);
-
             paint.setColor(Color.BLUE);
+            paint.setStrokeWidth(10);
             paint.setTextSize(30);
-            canvas.drawText("Text Left",150, 500, paint);
 
-            paint.setTextAlign(Paint.Align.CENTER);
-            canvas.drawText("Text Center",150, 525, paint);
+            stringBuilder
+                    .append("width = ").append(canvas.getWidth())
+                    .append(", height = ").append(canvas.getHeight());
+            canvas.drawText(stringBuilder.toString(), 100, 100, paint);
 
-            paint.setTextAlign(Paint.Align.RIGHT);
-            canvas.drawText("Text Right",150, 550, paint);
+            paint.setStyle(Paint.Style.FILL);
+            canvas.drawRect(rect, paint);
+
+            paint.setStyle(Paint.Style.STROKE);
+            rect.offset(150, 0);
+            canvas.drawRect(rect, paint);
+
+            paint.setStyle(Paint.Style.FILL_AND_STROKE);
+            rect.offset(150, 0);
+            canvas.drawRect(rect, paint);
         }
     }
 }
