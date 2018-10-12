3fc52bb097c31854a794a62817d9fe94b0c11619
Maxim Obrubov` <obrubov.max@mail.ru>
2018-10-12 17:10:08 +0300

    Индивидуальное задание. Рисование многоугольника

--- lab6_1/app/src/main/java/ru/obrubov/laba6_1/MainActivity.java
+++ lab6_1/app/src/main/java/ru/obrubov/laba6_1/MainActivity.java
@@ -4,8 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Rect;
-import android.graphics.RectF;
+import android.graphics.Path;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,38 +19,34 @@ public class MainActivity extends AppCompatActivity {
 
     class DrawView extends View {
         Paint paint;
-        Rect rect;
-        StringBuilder stringBuilder;
 
         public DrawView(Context context) {
             super(context);
             paint = new Paint();
-            rect = new Rect(100, 200, 200, 300);
-            stringBuilder = new StringBuilder();
         }
 
         @Override
         protected void onDraw(Canvas canvas) {
             canvas.drawARGB(80, 102, 204, 255);
-            paint.setColor(Color.BLUE);
+            paint.setColor(Color.YELLOW);
             paint.setStrokeWidth(10);
             paint.setTextSize(30);
 
-            stringBuilder
-                    .append("width = ").append(canvas.getWidth())
-                    .append(", height = ").append(canvas.getHeight());
-            canvas.drawText(stringBuilder.toString(), 100, 100, paint);
+            int centerX = canvas.getWidth() / 2;
+            int centerY = canvas.getHeight() / 2;
+            int startDegree = 0;
+            int anglesCount = 6;
+            int radius = 300;
+            float arcAngle = ((float) Math.PI *2) / anglesCount;
 
-            paint.setStyle(Paint.Style.FILL);
-            canvas.drawRect(rect, paint);
+            canvas.translate(centerX, centerY);
+            canvas.rotate(startDegree);
 
-            paint.setStyle(Paint.Style.STROKE);
-            rect.offset(150, 0);
-            canvas.drawRect(rect, paint);
-
-            paint.setStyle(Paint.Style.FILL_AND_STROKE);
-            rect.offset(150, 0);
-            canvas.drawRect(rect, paint);
+            Path path = new Path();
+            for(int i = 0; i <= anglesCount; i++) {
+                path.lineTo(radius * (float) Math.cos(arcAngle * i), radius * (float) Math.sin(arcAngle * i));
+            }
+            canvas.drawPath(path, paint);
         }
     }
 }
