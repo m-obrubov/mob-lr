1b4054ac0dc1f16a5676a1ceed2d083d8a69605a
Maxim Obrubov` <obrubov.max@mail.ru>
2018-10-30 17:19:52 +0300

    Реализа задания со скриншотами.Не работает

--- lab8/app/src/main/java/ru/obrubov/laba8/MainActivity.java
+++ lab8/app/src/main/java/ru/obrubov/laba8/MainActivity.java
@@ -1,171 +1,125 @@
 package ru.obrubov.laba8;
 
-import android.graphics.Matrix;
-import android.graphics.RectF;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.hardware.Camera;
-import android.hardware.Camera.CameraInfo;
-import android.hardware.Camera.Size;
+import android.hardware.Camera.PictureCallback;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Display;
-import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 
-import java.io.IOException;
-
 public class MainActivity extends AppCompatActivity {
-
-    SurfaceView sv;
-    SurfaceHolder holder;
-    HolderCallback holderCallback;
-    Camera camera;
-
-    final int CAMERA_ID = 0;
-    final boolean FULL_SCREEN = true;
+    private Camera camera;
+    private SurfaceHolder surfaceHolder;
+    private SurfaceView preview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_main);
 
-        sv = findViewById(R.id.surfaceView);
-        holder = sv.getHolder();
-        holderCallback = new HolderCallback();
-        holder.addCallback(holderCallback);
+        setContentView(R.layout.activity_main);
+        preview = findViewById(R.id.surfaceView);
+
+        surfaceHolder = preview.getHolder();
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        surfaceHolder.addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try
+                {
+                    camera.setPreviewDisplay(holder);
+                }
+                catch (IOException e)
+                {
+                    e.printStackTrace();
+                }
+
+                Camera.Size previewSize = camera.getParameters().getPreviewSize();
+                float aspect = (float) previewSize.width / previewSize.height;
+
+                int previewSurfaceWidth = preview.getWidth();
+                int previewSurfaceHeight = preview.getHeight();
+
+                ViewGroup.LayoutParams lp = preview.getLayoutParams();
+
+                // здесь корректируем размер отображаемого preview, чтобы не было искажений
+
+                if (getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE)
+                {
+                    // портретный вид
+                    camera.setDisplayOrientation(90);
+                    lp.height = previewSurfaceHeight;
+                    lp.width = (int) (previewSurfaceHeight / aspect);
+                }
+                else
+                {
+                    // ландшафтный
+                    camera.setDisplayOrientation(0);
+                    lp.width = previewSurfaceWidth;
+                    lp.height = (int) (previewSurfaceWidth / aspect);
+                }
+                preview.setLayoutParams(lp);
+                camera.startPreview();
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+
+            }
+        });
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        camera = Camera.open(CAMERA_ID);
-        setPreviewSize(FULL_SCREEN);
+        camera = Camera.open();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         if (camera != null)
-            camera.release();
-        camera = null;
-    }
-
-    class HolderCallback implements SurfaceHolder.Callback {
-
-        @Override
-        public void surfaceCreated(SurfaceHolder holder) {
-            try {
-                camera.setPreviewDisplay(holder);
-                camera.startPreview();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public void surfaceChanged(SurfaceHolder holder, int format, int width,
-                                   int height) {
+        {
             camera.stopPreview();
-            setCameraDisplayOrientation(CAMERA_ID);
-            try {
-                camera.setPreviewDisplay(holder);
+            camera.release();
+            camera = null;
+        }
+    }
+
+    public void onSavePicture(View view) {
+        camera.takePicture(null, null, null, new PictureCallback() {
+            @Override
+            public void onPictureTaken(byte[] data, Camera camera) {
+                try
+                {
+                    File pictures = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+                    File photoFile = new File(pictures, String.format("%d.png", System.currentTimeMillis()));
+                    FileOutputStream os = new FileOutputStream(photoFile);
+                    os.write(data);
+                    os.close();
+                }
+                catch (Exception ignored)
+                {
+                }
+
+                // после того, как снимок сделан, показ превью отключается. необходимо включить его
                 camera.startPreview();
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-        }
-
-        @Override
-        public void surfaceDestroyed(SurfaceHolder holder) {
-
-        }
-
-    }
-
-    void setPreviewSize(boolean fullScreen) {
-
-        // получаем размеры экрана
-        Display display = getWindowManager().getDefaultDisplay();
-        boolean widthIsMax = display.getWidth() > display.getHeight();
-
-        // определяем размеры превью камеры
-        Size size = camera.getParameters().getPreviewSize();
-
-        RectF rectDisplay = new RectF();
-        RectF rectPreview = new RectF();
-
-        // RectF экрана, соотвествует размерам экрана
-        rectDisplay.set(0, 0, display.getWidth(), display.getHeight());
-
-        // RectF первью
-        if (widthIsMax) {
-            // превью в горизонтальной ориентации
-            rectPreview.set(0, 0, size.width, size.height);
-        } else {
-            // превью в вертикальной ориентации
-            rectPreview.set(0, 0, size.height, size.width);
-        }
-
-        Matrix matrix = new Matrix();
-        // подготовка матрицы преобразования
-        if (!fullScreen) {
-            // если превью будет "втиснут" в экран (второй вариант из урока)
-            matrix.setRectToRect(rectPreview, rectDisplay,
-                    Matrix.ScaleToFit.START);
-        } else {
-            // если экран будет "втиснут" в превью (третий вариант из урока)
-            matrix.setRectToRect(rectDisplay, rectPreview,
-                    Matrix.ScaleToFit.START);
-            matrix.invert(matrix);
-        }
-        // преобразование
-        matrix.mapRect(rectPreview);
-
-        // установка размеров surface из получившегося преобразования
-        sv.getLayoutParams().height = (int) (rectPreview.bottom);
-        sv.getLayoutParams().width = (int) (rectPreview.right);
-    }
-
-    void setCameraDisplayOrientation(int cameraId) {
-        // определяем насколько повернут экран от нормального положения
-        int rotation = getWindowManager().getDefaultDisplay().getRotation();
-        int degrees = 0;
-        switch (rotation) {
-            case Surface.ROTATION_0:
-                degrees = 0;
-                break;
-            case Surface.ROTATION_90:
-                degrees = 90;
-                break;
-            case Surface.ROTATION_180:
-                degrees = 180;
-                break;
-            case Surface.ROTATION_270:
-                degrees = 270;
-                break;
-        }
-
-        int result = 0;
-
-        // получаем инфо по камере cameraId
-        CameraInfo info = new CameraInfo();
-        Camera.getCameraInfo(cameraId, info);
-
-        // задняя камера
-        if (info.facing == CameraInfo.CAMERA_FACING_BACK) {
-            result = ((360 - degrees) + info.orientation);
-        } else
-            // передняя камера
-            if (info.facing == CameraInfo.CAMERA_FACING_FRONT) {
-                result = ((360 - degrees) - info.orientation);
-                result += 360;
-            }
-        result = result % 360;
-        camera.setDisplayOrientation(result);
+        });
     }
 }
